951ccabb8dad5abddadc0abb6e48f9ec2a6c3928
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-12-15 17:11:15 +0900

    ♻️ Change the abbreviation to the full name and use flex gap for setting layout

--- counter/counter.js
+++ counter/counter.js
@@ -1,14 +1,16 @@
-const minusBtn = document.getElementById('minus');
-const plusBtn = document.getElementById('plus');
-const countText = document.getElementById('cnt');
+// TODO: + 버튼을 눌렀을 때 값이 1 증가, - 버튼을 눌렀을 때 값이 1 감소
+
+const minusButton = document.getElementById("minus_button");
+const plusButton = document.getElementById("plus_button");
+const counter = document.getElementById("counter");
 
 // click 이벤트가 발생 -> 어떤 동작 실행
 // 여기서 말하는 동작 -> 함수
 // 이 함수는 countText 안에 있는 숫자를 가져와서, +1 -1
 
-minusBtn.addEventListener('click', () => {
-  countText.innerText = Number(countText.innerText) - 1;
-})
-plusBtn.addEventListener('click', () => {
-  countText.innerText = Number(countText.innerText) + 1;
-})
+minusButton.addEventListener("click", () => {
+  counter.innerText = Number(counter.innerText) - 1;
+});
+plusButton.addEventListener("click", () => {
+  counter.innerText = Number(counter.innerText) + 1;
+});
