5a0d3ade2e8c1471699076bd6e9d59ae8055a109
Yeonu-Kim <ywk0524@snu.ac.kr>
2024-12-16 09:09:48 +0900

    🚚 Change the file name of font.html to index.html

--- font/font.js
+++ font/font.js
@@ -3,6 +3,8 @@ const textContainer = document.querySelector(".text_container");
 
 const texts = textContainer.children;
 
+// console.log(texts);
+
 slider.addEventListener("input", (event) => {
   // console.log(event);
   Array.from(texts).map((text) => {
